68348d8c2148888c84847373995e079c95b7e8c8
joshuaavalon <joshuaavalon@gmail.com>
2015-10-07 18:41:17 +0800

    Core

---
+++ 7zipper/Zipper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace _7zipper
+{
+    class Zipper
+    {
+        public string OutputPath;
+        public delegate void ZipperProgressUpdate(object sender, ProgressEventArgs e);
+        public event ZipperProgressUpdate ProgressState;
+
+        private readonly List<string> _filesList;
+
+        Zipper()
+        {
+            _filesList = new List<string>();
+            // Default path
+            OutputPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+        }
+
+        public void AddFile(params string[] files)
+        {
+            // Add unique file
+            foreach (var file in files.Where(file => !_filesList.Contains(file)))
+            {
+                _filesList.Add(file);
+            }
+        }
+
+        public void Clear()
+        {
+            _filesList.Clear();
+        }
+
+        public void Execute()
+        {
+            var count = 0;
+            foreach (var file in _filesList)
+            {
+                ReportProgress(count * 100 / _filesList.Count);
+                var name = Path.GetFileName(file);
+                CompressFileLzma(file, OutputPath + name);
+                count++;
+            }
+            ReportProgress(100);
+        }
+
+        private static void CompressFileLzma(string inFile, string outFile)
+        {
+            var coder = new SevenZip.Compression.LZMA.Encoder();
+            var input = new FileStream(inFile, FileMode.Open);
+            var output = new FileStream(outFile, FileMode.Create);
+
+            // Write the encoder properties
+            coder.WriteCoderProperties(output);
+
+            // Write the decompressed file size.
+            output.Write(BitConverter.GetBytes(input.Length), 0, 8);
+
+            // Encode the file.
+            coder.Code(input, output, input.Length, -1, null);
+            output.Flush();
+            output.Close();
+        }
+
+        private void ReportProgress(int progress)
+        {
+            var args = new ProgressEventArgs(progress);
+            ProgressState?.Invoke(this, args);
+        }
+    }
+
+    public class ProgressEventArgs : EventArgs
+    {
+        public int Progress { get; private set; }
+        public ProgressEventArgs(int progress)
+        {
+            Progress = progress;
+        }
+    }
+}
